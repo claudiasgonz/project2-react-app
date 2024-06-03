250cc50bc6c9983232c8c21e33fc510a815d8924
Claudia G <claudsain@gmail.com>
2024-06-03 14:01:17 -0400

    added dummy search bar to home page :bricks:

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,6 +7,11 @@ function About() {
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </p>
+        {/* <img
+        className="about-img"
+        alt="about page image"
+        src="https://i.pinimg.com/474x/db/67/dc/db67dc14c4e2c009df42f1ea2adcd44f.jpg"
+        /> */}
     </div>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,12 @@ function Homepage() {
   return (
     <div>
       <h1>Home page!!</h1>
+      
+      <div className="search-bar-home">
+        {/* SEARCH BAR */}
+        <h2>Explore</h2>
+        <input type="search" />
+      </div>
     </div>
   )
 }
