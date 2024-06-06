c736e8fad753a79a6d5d0f45804022db747fea7b
Claudia G <claudsain@gmail.com>
2024-06-06 13:33:38 -0400

    edited typo in about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,7 +8,7 @@ function About() {
           Garden.fy is a web application designed to help users learn about various plants that can be grown in Puerto Rico. The platform offers detailed information on the specific needs and care instructions for each plant, making it easier for gardening enthusiasts to cultivate a variety of plants. 
           <br/>
           <br/>
-         In addition to accesing a database of plants, users can share their experiences and contribute to the database, fostering a collaborative community of plant lovers. 💚🌱
+         In addition to accessing a database of plants, users can share their experiences and contribute to the database, fostering a collaborative community of plant lovers. 💚🌱
         </p>
     </div>
   )
